5beff525e49d5ff26e1d4e2c846fb2379cacc1c8
afruza-25 <118989989+afruza-25@users.noreply.github.com>
2022-12-03 21:20:13 +0500

    done

--- sum_odd.py
+++ sum_odd.py
@@ -3,10 +3,10 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 var_int=2389
 #Create a variable "sum_even" and assign it 0.
-sum_odd=0
+sum_even=0
 #Find the sum of the odd digits in the variable "var_int".
 a=var_int%10
 b=var_int//10%10
 c=var_int//100%10
 d=var_int//1000
-sum_odd=((a+1)%2*a+(b+1)%2*b+(c+1)%2*c+(d+1)%2*d)
+var_int=((a+1)%2*a+(b+1)%2*b+(c+1)%2*c+(d+1)%2*d)
